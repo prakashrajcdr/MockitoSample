5d87387c30e444493efd6bcbebfdf78833ade9a8
prakashrajcdr <prakashrajcdr@gmail.com>
2024-01-17 19:07:26 +0530

    updated

--- src/main/java/com/praksah/config/SecurityConfig.java
+++ src/main/java/com/praksah/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.praksah.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,5 +15,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        http.authorizeRequests()
                .anyRequest().authenticated()
                .and().oauth2Login();
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    	web.ignoring()
+    	.antMatchers("/api/getAllCustomers")
+    	.antMatchers("/api/addCustomer")
+    	.antMatchers("/api/getCustomer/{id}")
+    	.antMatchers("/api/updateCustomer/{id}")
+    	.antMatchers("/api/removeCustomer/{id}");
     }
 }
